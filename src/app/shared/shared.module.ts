043dc13c500a3846cdb2ee0183e7ecd884ab0cde
Dimitri Loukine <dlo@wine-logics.com>
2023-04-14 16:49:03 +0200

    fixed stackbitz issue

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { SliderComponent } from '@wyShared/components/slider/slider.component'; 
+import { SliderComponent } from './components/slider/slider.component'; 
+// import { SliderComponent } from '@wyShared/components/slider/slider.component'; 
 // NOTE: @wyShared est défini dans tsconfig.json, c'est une fonctionnalité de typescript indépendante d'angular,
-// ça résoud les problèmes de paths relatifs à rallonge.
+// ça résoud les problèmes de paths relatifs à rallonge, mais ça n'a pas l'air de marcher sur stackbitz
 // https://www.typescriptlang.org/tsconfig#paths
 
 
